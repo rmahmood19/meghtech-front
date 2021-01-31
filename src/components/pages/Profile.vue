8f97e00e38e850fbfc744b9badff01e32f42bfd1
Reaz <reaz.u.mahmood@gmail.com>
2021-01-31 01:40:42 +0100

    tailwind config

---
+++ src/components/pages/Profile.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="text-gray-100 h-96">
+    <p>
+      I am a full stack web developer with 3 years experience.
+    </p>
+    <p>
+      I am a full stack web developer with 3 years experience.
+    </p>
+    <p>
+      I am a full stack web developer with 3 years experience.
+    </p> <p>
+    I am a full stack web developer with 3 years experience.
+  </p> <p>
+    I am a full stack web developer with 3 years experience.
+  </p>
+
+
+
+
+  </div>
+
+
+</template>
+
+<script>
+export default {
+name: "Profile"
+}
+</script>
+
+<style scoped>
+
+</style>
